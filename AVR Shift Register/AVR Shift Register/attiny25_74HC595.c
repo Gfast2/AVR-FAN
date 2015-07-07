8cee0058c4d9a4f28a42d438138e2fc7dc70b4a3
Su <Gfast2@hotmail.com>
2015-07-07 12:48:17 +0200

    Works with Timer/Counter1 perfectly o@65KHz
    Final Beta before refactory.

--- AVR Shift Register/AVR Shift Register/attiny25_74HC595.c
+++ AVR Shift Register/AVR Shift Register/attiny25_74HC595.c
@@ -9,7 +9,7 @@
 
 // In first try it shows about 4 sec. per digit under 8000000UL. And I think the 8 divider (CKDI8) in fuse works.
 // This time I deactivate this divider fuse in order to speed up PWM (original with divided prescaler PWM working at 500K)
-#define F_CPU 8000000UL 
+#define F_CPU 1000000UL 
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -17,8 +17,6 @@
 #include <util/delay.h>
 
 volatile uint16_t voltage_reading;
-volatile uint8_t  firstdigit;
-volatile uint8_t  pwmFAN;
 volatile uint8_t  dutyCycle = 127; //percent value of PWM dutyCycle.
 
 void initialise_ADC(void)
@@ -41,17 +39,16 @@ int main(void)
 	start_conversion();
 	
 	DDRB |= (1 << PB0) | (1 << PB1);						// PWM control pin, as output
+	
+	//This block are using Timer/Counter0 to drive FAN and the LED @32.5KHz
 	//TCCR0A |= (1<<COM0A1) | (1<<COM0B1) | (1<<WGM00) | (1<<WGM01);	// fast PWM mode + clear OC0A & CO0B on compare match
-	
 	//TIMSK |= (1<<TOIE0);								// Timer/Counter0 Overflow Interrupt Enable
-	
 	//OCR0A = 125;										// should later be comment out to let interrupt handle this problem.
 	//OCR0B = 125;
-	
 	//TCCR0B |= (1<<CS00);								// no prescaler
 	
-	OCR1C = 159;
-	TCCR1 |= (1 << PWM1B) | (1 << PWM1A) | (1 << COM1A1) | (1 << CS11); // PWM mode + clear OC1A output line + CK/2
+	OCR1C = 127; // setting the PWM speed. In this case, it is working @100KHz
+	TCCR1 |= (1 << PWM1A) | (1 << COM1A0) /*| (1 << CS11)*/ | (1 << CS10); // PWM mode + clear OC1A output line + CK/2. COM1A0!! Page:87
 	
 	TIMSK |= (1 << TOIE1); //P.92 Timer/Counter Overflow Interrupt Enable
 	
@@ -64,7 +61,7 @@ ISR(ADC_vect)
 {
 	voltage_reading = ADC;
 	
-	dutyCycle = voltage_reading / 4; // voltage_reading: 0 ~ 1023, dutyCycle: 0 ~ 255
+	dutyCycle = voltage_reading / 8; // voltage_reading: 0 ~ 1023, dutyCycle: 0 ~ 255
 	ADCSRA |= 1<<ADSC;
 }
 
